c367789463d103b49a9c6becac15bf442f42e5ca
RUSHANK JAIN <jain_rushank@srmap.edu.in>
2021-05-26 02:33:15 +0530

    Add files via upload

---
+++ OOPS/virtual_class.cpp
@@ -0,0 +1,104 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+#define MAX_SIZE 1000001 + 1
+#define mp make_pair
+#define pb push_back
+#define fr first
+#define sc second
+#define MOD 1e9 + 7
+#define loop(i, n) for (int i = 0; i < n; i++)
+#define nloop(i, n) for (int i = n - 1; i >= 0; i--)
+#define veca(v) v.begin(), v.end()
+#define arra(a, n) a, a + n
+
+#include<iostream>
+using namespace std;
+/*
+Inheritance:
+student -->test [Done]
+student-->sports [Done]
+test --> result [Done]
+sports --> result [Done]
+*/
+
+class Student{
+    protected:
+        int roll_no;
+    public:
+        void set_number(int a){
+            roll_no = a;
+        }
+        void print_number(void){
+            cout<<"Your roll no is "<< roll_no<<endl;
+        }
+};
+
+class Test : virtual public Student{
+    protected:
+        float maths, physics;
+        public:
+            void set_marks(float m1, float m2){
+                maths = m1;
+                physics = m2;
+            }
+
+            void print_marks(void){
+                cout << "You result is here: "<<endl
+                     << "Maths: "<< maths<<endl
+                     << "Physics: "<< physics<<endl;
+            }
+};
+
+class Sports: virtual public Student{
+    protected:
+        float score;
+    public:
+        void set_score(float sc){
+            score = sc;
+        }
+
+        void print_score(void){
+            cout<<"Your PT score is "<<score<<endl;
+        }
+
+};
+
+class Result : public Test, public Sports{
+    private:
+        float total;
+    public:
+        void display(void){
+            total = maths + physics + score;
+            print_number();
+            print_marks();
+            print_score();
+            cout<< "Your total score is: "<<total<<endl;
+        }
+};
+
+
+
+void solve()
+{
+    Result harry;
+    harry.set_number(4200);
+    harry.set_marks(78.9, 99.5);
+    harry.set_score(9);
+    harry.display();
+}
+
+int main(){
+    
+    ios::sync_with_stdio(0);
+    cout.tie(0);
+    cin.tie(0);
+    int t = 1;
+    //cin>>t;
+    while (t--)
+    {
+    solve();
+    cout << endl;
+    }
+    return 0;
+}
